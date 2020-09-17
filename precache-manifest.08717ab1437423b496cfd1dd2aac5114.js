62afe9b5e767f676ac76cf1d2ffa556307ca8fbc
Adrian <eidiazcas@gmail.com>
2020-09-17 01:24:22 -0500

    Updates

---
+++ precache-manifest.08717ab1437423b496cfd1dd2aac5114.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cca179ecbaf74bca5be0e6a9c11662",
+    "url": "/code_alive_demo/index.html"
+  },
+  {
+    "revision": "7fbaa16d7bf4a6ad482d",
+    "url": "/code_alive_demo/static/css/2.4e451e98.chunk.css"
+  },
+  {
+    "revision": "91531df25b40bfbe870a",
+    "url": "/code_alive_demo/static/css/main.8b841aed.chunk.css"
+  },
+  {
+    "revision": "7fbaa16d7bf4a6ad482d",
+    "url": "/code_alive_demo/static/js/2.80b1b197.chunk.js"
+  },
+  {
+    "revision": "771475c9d08307656e35e0285c3863f2",
+    "url": "/code_alive_demo/static/js/2.80b1b197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91531df25b40bfbe870a",
+    "url": "/code_alive_demo/static/js/main.399dc093.chunk.js"
+  },
+  {
+    "revision": "4792f9acb6f2a7928edc",
+    "url": "/code_alive_demo/static/js/runtime-main.8ce2e8a2.js"
+  },
+  {
+    "revision": "79fcb18e4cc8dfd20fc0806173578f13",
+    "url": "/code_alive_demo/static/media/backward.79fcb18e.svg"
+  },
+  {
+    "revision": "b935f77bee5e4d087d02e8b3bcba7daa",
+    "url": "/code_alive_demo/static/media/edit.b935f77b.svg"
+  },
+  {
+    "revision": "6ac8ce0a0ec6b8a0c0bfd77c1069f718",
+    "url": "/code_alive_demo/static/media/fast.6ac8ce0a.svg"
+  },
+  {
+    "revision": "6c16e89f216e38f9f92630b667f42f60",
+    "url": "/code_alive_demo/static/media/film.6c16e89f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code_alive_demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7170d0f61ac205bed23ee895420c6072",
+    "url": "/code_alive_demo/static/media/pause.7170d0f6.svg"
+  },
+  {
+    "revision": "df00469d358c74d5d306e6cb225d8ad5",
+    "url": "/code_alive_demo/static/media/play_arrow.df00469d.svg"
+  },
+  {
+    "revision": "ef00aea9b772147fe96255bb35b3c1cf",
+    "url": "/code_alive_demo/static/media/skip_next.ef00aea9.svg"
+  },
+  {
+    "revision": "b41eb7942538497d2220e3b6586001ed",
+    "url": "/code_alive_demo/static/media/skip_previous.b41eb794.svg"
+  },
+  {
+    "revision": "83b251bd6ffabbcabd1daf95db21c18f",
+    "url": "/code_alive_demo/static/media/slow.83b251bd.svg"
+  }
+]);
